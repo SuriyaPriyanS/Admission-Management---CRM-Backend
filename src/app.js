69926a639ce4e191bafba30446359cdc9aac5b5d
Suriya Priyan <suriyapriyan0506@gmail.com>
2026-03-28 14:38:17 +0530

    updted

--- src/app.js
+++ src/app.js
@@ -13,9 +13,15 @@ dotenv.config();
 
 const app = express();
 const isProduction = process.env.NODE_ENV === "production";
+const corsOptions = {
+  origin: true,
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
 
 app.use(helmet());
-app.use(cors());
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
